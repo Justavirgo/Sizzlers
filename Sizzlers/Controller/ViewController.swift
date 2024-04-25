d41a268c46000c2fa726626d9002053546748ec2
Justin Elijah <justinelijah88@gmail.com>
2024-04-25 00:11:49 -0500

    Started preparing to move over to the second view controller

--- Sizzlers/Controller/ViewController.swift
+++ Sizzlers/Controller/ViewController.swift
@@ -8,21 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     let secondVC = SignUpViewController()
     
     @IBAction func goToSignUpButton(_ sender: UIButton) {
-        func performSegue(withIdentifier identifier: String, sender: Any?) {
-            let secondVC = SignUpViewController()
-        }
+        self.performSegue(withIdentifier: "goToSignUp", sender: self)
         
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "goToSignUp" {
+            let destinationVC = segue.destination as! SignUpViewController
+            
+        }
+    }
+    
 }
 
